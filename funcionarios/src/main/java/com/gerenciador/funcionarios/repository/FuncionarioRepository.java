f8ff15f352611b42225ad3d50070b7366f3c7c49
Gabriel Maiolli <bielmaiolli@gmail.com>
2025-02-18 12:57:23 -0300

    Update FuncionarioRepository.java

--- funcionarios/src/main/java/com/gerenciador/funcionarios/repository/FuncionarioRepository.java
+++ funcionarios/src/main/java/com/gerenciador/funcionarios/repository/FuncionarioRepository.java
@@ -2,7 +2,9 @@ package com.gerenciador.funcionarios.repository;
 
 import com.gerenciador.funcionarios.entities.Funcionario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 import java.util.Optional;
 
 public interface FuncionarioRepository extends MongoRepository<Funcionario, String> {
